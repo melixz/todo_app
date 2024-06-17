692d974aab0f62a295cdd38e07bc2758dae5ef59
Vlad <dr.melix@gmail.com>
2024-06-18 00:16:29 +0300

    - Добавлены теги и описания к маршрутам в routers.py - Обновлен файл main.py для настройки основного приложения с заголовком, описанием и версией - Маршруты теперь организованы по тегам для улучшения читаемости и навигации в Swagger UI

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from .routers import router
 
-app = FastAPI()
+app = FastAPI(
+    title="ToDo Application",
+    description="API для управления задачами",
+    version="1.0.0",
+)
 
 app.include_router(router)
--- app/routers.py
+++ app/routers.py
@@ -8,23 +8,23 @@ router = APIRouter()
 tasks: List[Task] = []
 
 
-@router.get("/")
+@router.get("/", tags=["Root"], summary="Приветственное сообщение")
 def read_root():
     return {"message": "Welcome to the ToDo App!"}
 
 
-@router.post("/tasks/", response_model=Task)
+@router.post("/tasks/", response_model=Task, tags=["Tasks"], summary="Создание новой задачи")
 def create_task(task: Task):
     tasks.append(task)
     return task
 
 
-@router.get("/tasks/", response_model=List[Task])
+@router.get("/tasks/", response_model=List[Task], tags=["Tasks"], summary="Получение списка всех задач")
 def get_tasks():
     return tasks
 
 
-@router.put("/tasks/{task_id}", response_model=Task)
+@router.put("/tasks/{task_id}", response_model=Task, tags=["Tasks"], summary="Обновление задачи по ID")
 def update_task(task_id: int, updated_task: Task):
     task = find_task(tasks, task_id)
     if task is None:
@@ -36,7 +36,7 @@ def update_task(task_id: int, updated_task: Task):
     return task
 
 
-@router.delete("/tasks/{task_id}", response_model=Task)
+@router.delete("/tasks/{task_id}", response_model=Task, tags=["Tasks"], summary="Удаление задачи по ID")
 def delete_task(task_id: int):
     task = find_task(tasks, task_id)
     if task is None:
